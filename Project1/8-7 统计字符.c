e48d1e2a1ae267a32b1500c9dda43081ff9b65f1
Nonac <nonacyang@gmail.com>
2019-04-15 09:10:44 +0800

    函数

--- Project1/8-7 统计字符.c
+++ Project1/8-7 统计字符.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
 void main()
 {
-	int num[10] = { 0 }, blank = 0, others = 0,a;
+	int num[10] = { 0 }, blank = 0, others = 0, a = 0;
 	printf("input a dozens of charators:\n");
-	while (1)
+	while (a != EOF)
 	{
 		a = getchar();
-		printf("%c", a);
-		if (a == 27) break;
-		a > 47 && a < 58 ? num[a - 48]++ : 0;
-		a == 32 || a == 9 || a == 10 ? blank++ : 0;
-		others++;
+		a > 47 && a < 58 ? num[a - 48]++ : a == 32 || a == 9 || a == 10 ? blank++: others++;
 	}
-	for (size_t i = 0; i < 11; i++)
+	for (size_t i = 0; i < 10; i++)
 	{
-		if (num[i] != 0)printf("There are %d %d in this paragraph.", num[i], i);
+		if (num[i] != 0) printf("There are %d %d in this paragraph.\n", num[i], i);
 	}
-	printf("There are %d blank in this paragraph.", blank);
-	printf("There are %d others in this paragraph.", others);
+	printf("There are %d blank in this paragraph.\n", blank);
+	printf("There are %d others in this paragraph.\n", others);
+	system("pause");
 }
