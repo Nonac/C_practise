439473d647192a5e83c8a0bb316bd391dab8c8bd
Nonac <nonacyang@gmail.com>
2019-04-16 15:01:50 +0800

    指针

--- Project1/9-3 数组中n个元素倒序存放.c
+++ Project1/9-3 数组中n个元素倒序存放.c
@@ -14,16 +14,22 @@ void main()
 		scanf_s("%d", &a[i]);
 		i++;
 	}
-
-	for (i = 0; i <= ((n - 1) / 2); i++)
-	{
-		swap(&a[i], &a[n - 1 - i]);
-	}
+	int exchange(int a[], int n);
+	exchange(a, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d\n", a[i]);
 	}
 	free(a);
+}
+int exchange(int a[ ], int n)
+{
+	int temp=0,i = 0;
+	extern void swap(int*, int*);
+	for (i = 0; i <= ((n-1) / 2); i++)
+	{
+			swap(&a[i], &a[n - 1 - i]);
+	}
 }
 void swap(int* x, int* y)
 {
