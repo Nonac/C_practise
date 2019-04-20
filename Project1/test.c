6bc3d013eec4431504e2738fa996d2325cc99cb1
Nonac <nonacyang@gmail.com>
2019-04-20 18:49:33 +0800

    异或运算会引起gcc的内存报错

--- Project1/test.c
+++ Project1/test.c
@@ -3,7 +3,8 @@
 #include<stdlib.h>
 void main()
 {
-	char str =  "459684616546" ,int_max = "\0";
-	sprintf_s(int_max, INT_MAX, 10);
-	int_max < str ? printf("yes") : printf("no");
+	int len = 3;
+	char* str = (char*)malloc(len * sizeof(char));
+	str[0] = '0';
+	printf("%c",str[0]);
 }
