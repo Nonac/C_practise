0817da3fa137812ba615bc91b10698c1c5f12d88
Nonac <nonacyang@gmail.com>
2019-07-29 20:11:08 +0800

    EightQueen

---
+++ history/汉诺塔.c
@@ -0,0 +1,43 @@
+//
+// Created by Nick Yang on 2019-07-29.
+//
+
+#include <stdio.h>
+#include <time.h>
+
+int i=1;
+
+void printi(char x,char y)
+{
+    printf("%d.%c-->%c\n",i,x,y);
+    i++;
+}
+
+void move(int n,int i,char x,char y,char z)
+{
+    if (n == 1)
+    {
+        printi(x,z);
+    } else
+    {
+        move(n-1,i,x,z,y);
+        printi(x,z);
+        move(n-1,i,y,x,z);
+    }
+}
+
+int main()
+{
+    clock_t startTime,endTime;
+    startTime = clock();
+    int n;
+    printf("请输入汉诺塔的层数：");
+    scanf("%d",&n);
+    printf("移动的步骤如下:\n");
+
+    move(n,i,'X','Y','Z');
+
+    endTime = clock();
+    printf("Total time is:%lf",(double)(endTime - startTime) / CLOCKS_PER_SEC);
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,39 +1,133 @@
 #include <stdio.h>
-#include <time.h>
 
-int i=1;
+int count = 0;
 
-void printi(char x,char y)
+int notDanger( int row, int j, int (*chess)[8] )
 {
-    printf("%d.%c-->%c\n",i,x,y);
-    i++;
+    int i, k, flag1=0, flag2=0, flag3=0, flag4=0, flag5=0;
+
+    // 判断列方向
+    for( i=0; i < 8; i++ )
+    {
+        if( *(*(chess+i)+j) != 0 )
+        {
+            flag1 = 1;
+            break;
+        }
+    }
+
+    // 判断左上方
+    for( i=row, k=j; i>=0 && k>=0; i--, k-- )
+    {
+        if( *(*(chess+i)+k) != 0 )
+        {
+            flag2 = 1;
+            break;
+        }
+    }
+
+    // 判断右下方
+    for( i=row, k=j; i<8 && k<8; i++, k++ )
+    {
+        if( *(*(chess+i)+k) != 0 )
+        {
+            flag3 = 1;
+            break;
+        }
+    }
+
+    // 判断右上方
+    for( i=row, k=j; i>=0 && k<8; i--, k++ )
+    {
+        if( *(*(chess+i)+k) != 0 )
+        {
+            flag4 = 1;
+            break;
+        }
+    }
+
+    // 判断左下方
+    for( i=row, k=j; i<8 && k>=0; i++, k-- )
+    {
+        if( *(*(chess+i)+k) != 0 )
+        {
+            flag5 = 1;
+            break;
+        }
+    }
+
+    if( flag1 || flag2 || flag3 || flag4 || flag5 )
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
 
-void move(int n,int i,char x,char y,char z)
+// 参数row: 表示起始行
+// 参数n: 表示列数
+// 参数(*chess)[8]: 表示指向棋盘每一行的指针
+void EightQueen( int row, int n, int (*chess)[8] )
 {
-    if (n == 1)
+    int chess2[8][8], i, j;
+
+    for( i=0; i < 8; i++ )
     {
-        printi(x,z);
-    } else
+        for( j=0; j < 8; j++ )
+        {
+            chess2[i][j] = chess[i][j];
+        }
+    }
+
+    if( 8 == row )
     {
-        move(n-1,i,x,z,y);
-        printi(x,z);
-        move(n-1,i,y,x,z);
+        printf("第 %d 种\n", count++);
+        for( i=0; i < 8; i++ )
+        {
+            for( j=0; j < 8; j++ )
+            {
+                printf("%d ", *(*(chess2+i)+j));
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    else
+    {
+        for( j=0; j < n; j++ )
+        {
+            if( notDanger( row, j, chess ) ) // 判断是否危险
+            {
+                for( i=0; i < 8; i++ )
+                {
+                    *(*(chess2+row)+i) = 0;
+                }
+
+                *(*(chess2+row)+j) = 1;
+
+                EightQueen( row+1, n, chess2 );
+            }
+        }
     }
 }
 
 int main()
 {
-    clock_t startTime,endTime;
-    startTime = clock();
-    int n;
-    printf("请输入汉诺塔的层数：");
-    scanf("%d",&n);
-    printf("移动的步骤如下:\n");
+    int chess[8][8], i, j;
 
-    move(n,i,'X','Y','Z');
+    for( i=0; i < 8; i++ )
+    {
+        for( j=0; j < 8; j++ )
+        {
+            chess[i][j] = 0;
+        }
+    }
 
-    endTime = clock();
-    printf("Total time is:%lf",(double)(endTime - startTime) / CLOCKS_PER_SEC);
+    EightQueen( 0, 8, chess );
+
+    printf("总共有 %d 种解决方法!\n\n", count);
+
     return 0;
 }
